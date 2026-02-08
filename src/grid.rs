3993c7bb59fdcaf8da2eb1290ae8b81a986f9341
CoolModderJaydon <131696751+JaydonXOneGitHub@users.noreply.github.com>
2026-02-07 23:39:58 -0500

    Added button style callback to Grid

--- src/grid.rs
+++ src/grid.rs
@@ -1,15 +1,28 @@
 use std::marker::PhantomData;
 
-use iced::{Element, Padding, Task, widget::{Column, Row, container, scrollable}};
+use iced::{Element, Padding, Task, Theme, widget::{Column, Row, button::{Status, Style}, container, scrollable}};
 
 use crate::{TGridButton, direction::Direction, message::Message, position::Position};
 use iced::widget::button as button_fn;
 
+/// Ideal structure for member names is:
+/// (grid, x, y, theme, status, grid_button)
+pub type ButtonCallback<CustomMessage, GridButton> = Box<dyn Fn(
+    &Grid<CustomMessage, GridButton>,
+    usize, 
+    usize, 
+    &Theme,
+    Status,
+    &GridButton
+) -> Style>;
+
+/// The main struct for the [`Environment`]'s navigation
 pub struct Grid<CustomMessage, GridButton: TGridButton> {
     pub locations: Vec<Vec<GridButton>>,
     pub position: Position,
     pub tile_size: f32,
     pub spacing: f32,
+    pub button_callback: Option<ButtonCallback<CustomMessage, GridButton>>,
     _marker: PhantomData<CustomMessage>
 }
 
@@ -20,24 +33,34 @@ impl<CustomMessage, GridButton: TGridButton> Grid<CustomMessage, GridButton> {
             position: Position::zero(),
             tile_size: 0.0,
             spacing: 0.0,
+            button_callback: Option::None,
             _marker: PhantomData
         };
     }
 }
 
 impl<CustomMessage: Clone, GridButton: TGridButton> Grid<CustomMessage, GridButton> {
+    /// Set the tile size
     pub fn with_tile_size(mut self, tile_size: f32) -> Self {
         self.tile_size = tile_size;
         return self;
     }
 
+    /// Set the grid spacing
     pub fn with_spacing(mut self, spacing: f32) -> Self {
         self.spacing = spacing;
+        return self;
+    }
+
+    /// Set the button style callback
+    pub fn with_button_callback(mut self, callback: Option<ButtonCallback<CustomMessage, GridButton>>) -> Self {
+        self.button_callback = callback;
         return self;
     }
 }
 
 impl<CustomMessage: Clone, GridButton: TGridButton> Grid<CustomMessage, GridButton> {
+    /// Convert the data to the button elements
     pub fn to_element(&self) -> Element<'_, Message<CustomMessage>> {
         let rows: Vec<Element<'_, Message<CustomMessage>>> = self.locations.iter()
         .enumerate()
@@ -54,6 +77,17 @@ impl<CustomMessage: Clone, GridButton: TGridButton> Grid<CustomMessage, GridButt
                         )
                         .width(self.tile_size)
                         .height(self.tile_size)
+                        .style(|t, s| -> Style {
+                            return match &self.button_callback {
+                                Option::Some(callback) => callback(
+                                    self,
+                                    self.position.x,
+                                    self.position.y,
+                                    t, s, btn_data
+                                ),
+                                Option::None => Style::default()
+                            };
+                        })
                         .on_press(Message::ButtonPressed(Position::new(c, r)))
                     )
                     .center_x(self.tile_size)
